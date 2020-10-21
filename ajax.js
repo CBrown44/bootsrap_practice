46a6d356d1ef324e6f2162a9233f584a07666ffe
Caleb Brown <cbrown200@west-mec.org>
2020-10-21 16:42:01 -0700

    changed from jquery to xml

--- ajax.js
+++ ajax.js
@@ -1,44 +1,50 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://rapidapi.p.rapidapi.com/locations",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "escape-from-tarkov.p.rapidapi.com",
-		"x-rapidapi-key": "2934d3483emsh3ea1fbe8a0e5bddp17c441jsnd91e4b089144"
-	}
-};
+const data = null;
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
 });
 
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://rapidapi.p.rapidapi.com/currencies",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "escape-from-tarkov.p.rapidapi.com",
-		"x-rapidapi-key": "2934d3483emsh3ea1fbe8a0e5bddp17c441jsnd91e4b089144"
-	}
-};
+xhr.open("GET", "https://rapidapi.p.rapidapi.com/bosses");
+xhr.setRequestHeader("x-rapidapi-host", "escape-from-tarkov.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "SIGN-UP-FOR-KEY");
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
+xhr.send(data);
+
+const data = null;
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
 });
 
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://rapidapi.p.rapidapi.com/bosses",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "escape-from-tarkov.p.rapidapi.com",
-		"x-rapidapi-key": "2934d3483emsh3ea1fbe8a0e5bddp17c441jsnd91e4b089144"
-	}
-};
+xhr.open("GET", "https://rapidapi.p.rapidapi.com/locations");
+xhr.setRequestHeader("x-rapidapi-host", "escape-from-tarkov.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "2934d3483emsh3ea1fbe8a0e5bddp17c441jsnd91e4b089144");
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
+xhr.send(data);
+
+const data = null;
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
 });
+
+xhr.open("GET", "https://rapidapi.p.rapidapi.com/currencies");
+xhr.setRequestHeader("x-rapidapi-host", "escape-from-tarkov.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "2934d3483emsh3ea1fbe8a0e5bddp17c441jsnd91e4b089144");
+
+xhr.send(data);
