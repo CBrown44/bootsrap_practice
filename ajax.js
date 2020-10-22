8f6ba8c5d2b161f7e830ec1cf9f92c93613d0948
Caleb Brown <cbrown200@west-mec.org>
2020-10-21 17:05:04 -0700

    renamed the xhr for location and currency

--- ajax.js
+++ ajax.js
@@ -21,36 +21,36 @@ xhr.send(bossData);
 //locations in game
 const locationData = null;
 
-const xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
+const rhr = new XMLHttpRequest();
+rhr.withCredentials = true;
 
-xhr.addEventListener("readystatechange", function () {
+rhr.addEventListener("readystatechange", function () {
 	if (this.readyState === this.DONE) {
 		console.log(this.responseText);
 	}
 });
 
-xhr.open("GET", "https://rapidapi.p.rapidapi.com/locations");
-xhr.setRequestHeader("x-rapidapi-host", "escape-from-tarkov.p.rapidapi.com");
-xhr.setRequestHeader("x-rapidapi-key", "2934d3483emsh3ea1fbe8a0e5bddp17c441jsnd91e4b089144");
+rhr.open("GET", "https://rapidapi.p.rapidapi.com/locations");
+rhr.setRequestHeader("x-rapidapi-host", "escape-from-tarkov.p.rapidapi.com");
+rhr.setRequestHeader("x-rapidapi-key", "2934d3483emsh3ea1fbe8a0e5bddp17c441jsnd91e4b089144");
 
-xhr.send(locationData);
+rhr.send(locationData);
 
 
 //currency in game
 const moneyData = null;
 
-const xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
+const bhr = new XMLHttpRequest();
+bhr.withCredentials = true;
 
-xhr.addEventListener("readystatechange", function () {
+bhr.addEventListener("readystatechange", function () {
 	if (this.readyState === this.DONE) {
 		console.log(this.responseText);
 	}
 });
 
-xhr.open("GET", "https://rapidapi.p.rapidapi.com/currencies");
-xhr.setRequestHeader("x-rapidapi-host", "escape-from-tarkov.p.rapidapi.com");
-xhr.setRequestHeader("x-rapidapi-key", "2934d3483emsh3ea1fbe8a0e5bddp17c441jsnd91e4b089144");
+bhr.open("GET", "https://rapidapi.p.rapidapi.com/currencies");
+bhr.setRequestHeader("x-rapidapi-host", "escape-from-tarkov.p.rapidapi.com");
+bhr.setRequestHeader("x-rapidapi-key", "2934d3483emsh3ea1fbe8a0e5bddp17c441jsnd91e4b089144");
 
-xhr.send(moneyData);
+bhr.send(moneyData);
