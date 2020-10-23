8a70f56e3757c0366aba3228b759c863f97eeeed
Daniel Garcia <dgarcia300@west-mec.org>
2020-10-22 17:58:11 -0700

    changed out locations for dealers and added variables

--- ajax.js
+++ ajax.js
@@ -1,6 +1,22 @@
 //bosses in game
 const bossData = null;
 
+const bossName1 = document.querySelector('.bossName1')
+const spawnrate1 = document.querySelector('.spawnRate1')
+const follower1 = document.querySelector('.follower1')
+
+const bossName2 = document.querySelector('.bossName2')
+const spawnrate2 = document.querySelector('.spawnRate2')
+const follower2 = document.querySelector('.follower2')
+
+const bossName3 = document.querySelector('.bossName3')
+const spawnrate3 = document.querySelector('.spawnRate3')
+const follower3 = document.querySelector('.follower3')
+
+const bossName4 = document.querySelector('.bossName4')
+const spawnrate4 = document.querySelector('.spawnRate4')
+const follower4 = document.querySelector('.follower4')
+
 const xhr = new XMLHttpRequest();
 xhr.withCredentials = true;
 
@@ -17,23 +33,39 @@ xhr.setRequestHeader("x-rapidapi-key", "SIGN-UP-FOR-KEY");
 xhr.send(bossData);
 
 
-//locations in game
-const locationData = null;
+//Dealers in game
+const data = null;
 
-const rhr = new XMLHttpRequest();
-rhr.withCredentials = true;
+const dealerName1 = document.querySelector('.dealerName1')
+const realName1 = document.querySelector('.realName1')
+const origin1 = document.querySelector('.origin1')
 
-rhr.addEventListener("readystatechange", function () {
+const dealerName2 = document.querySelector('.dealerName2')
+const realName2 = document.querySelector('.realName2')
+const origin2 = document.querySelector('.origin2')
+
+const dealerName3 = document.querySelector('.dealerName3')
+const realName3 = document.querySelector('.realName3')
+const origin3 = document.querySelector('.origin3')
+
+const dealerName4 = document.querySelector('.dealerName4')
+const realName4 = document.querySelector('.realName4')
+const origin4 = document.querySelector('.origin4')
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
 	if (this.readyState === this.DONE) {
 		console.log(this.responseText);
 	}
 });
 
-rhr.open("GET", "https://rapidapi.p.rapidapi.com/locations");
-rhr.setRequestHeader("x-rapidapi-host", "escape-from-tarkov.p.rapidapi.com");
-rhr.setRequestHeader("x-rapidapi-key", "2934d3483emsh3ea1fbe8a0e5bddp17c441jsnd91e4b089144");
+xhr.open("GET", "https://rapidapi.p.rapidapi.com/dealers");
+xhr.setRequestHeader("x-rapidapi-host", "escape-from-tarkov.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "40ea6137eemsh9ce3a93415e7d0ap17fac4jsndf9a819d6dde");
 
-rhr.send(locationData);
+xhr.send(data);
 
 
 //currency in game
